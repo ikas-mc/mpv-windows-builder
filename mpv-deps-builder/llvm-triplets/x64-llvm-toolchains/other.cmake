1195cef9dab1e21d20d16249579ed44688636de7
ikas <65895428+ikas-mc@users.noreply.github.com>
2026-05-04 16:11:24 +0800

    remove ffmpeg gpl

--- mpv-deps-builder/llvm-triplets/x64-llvm-toolchains/other.cmake
+++ mpv-deps-builder/llvm-triplets/x64-llvm-toolchains/other.cmake
@@ -2,3 +2,13 @@ if(PORT MATCHES "^(spirv-tools)$")
     set(VCPKG_CXX_FLAGS "-Wno-error=unused-command-line-argument")
     set(VCPKG_C_FLAGS "-Wno-error=unused-command-line-argument")
 endif()
+
+if(PORT MATCHES "^(opus)$")
+    set(VCPKG_CXX_FLAGS "-mssse3 -msse4.1")
+    set(VCPKG_C_FLAGS "-mssse3 -msse4.1")
+endif()
+
+if(PORT MATCHES "^(libvpx)$")
+    set(VCPKG_CXX_FLAGS "-msse3 -mssse3 -msse4.1 -msse4.2 -mavx -mavx2")
+    set(VCPKG_C_FLAGS "-msse3 -mssse3 -msse4.1 -msse4.2 -mavx -mavx2")
+endif()
